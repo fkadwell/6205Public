8f74fbf6dd5d73c5ee2b201b1858f20eff3e784f
fkadwell <fkadwell@d3informationservices.com>
2023-03-16 11:42:28 -0500

    Sample Code

--- Sample/src/LookupIndex.java
+++ Sample/src/LookupIndex.java
@@ -13,44 +13,6 @@
  */
 public class LookupIndex {
 
-    // Do not instantiate.
-    private LookupIndex() { }
-
-    public static void main(String[] args) {
-        String filename  = args[0];
-        String separator = args[1];
-        In in = new In(filename);
-
-        ST<String, Queue<String>> st = new ST<String, Queue<String>>();
-        ST<String, Queue<String>> ts = new ST<String, Queue<String>>();
-
-        while (in.hasNextLine()) {
-            String line = in.readLine();
-            String[] fields = line.split(separator);
-            String key = fields[0];
-            for (int i = 1; i < fields.length; i++) {
-                String val = fields[i];
-                if (!st.contains(key)) st.put(key, new Queue<String>());
-                if (!ts.contains(val)) ts.put(val, new Queue<String>());
-                st.get(key).enqueue(val);
-                ts.get(val).enqueue(key);
-            }
-        }
-
-        StdOut.println("Done indexing");
-
-        // read queries from standard input, one per line
-        while (!StdIn.isEmpty()) {
-            String query = StdIn.readLine();
-            if (st.contains(query))
-                for (String vals : st.get(query))
-                    StdOut.println("  " + vals);
-            if (ts.contains(query))
-                for (String keys : ts.get(query))
-                    StdOut.println("  " + keys);
-        }
-
-    }
 
 }
 
